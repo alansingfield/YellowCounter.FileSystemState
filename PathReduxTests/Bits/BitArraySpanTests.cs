af8d8930c4d1f4a16ab14d283c9dfd3b3e1e323e
Alan Singfield <alan.singfield.work@ntlworld.com>
2020-06-27 11:05:15 +0100

    Made a Span equivalent for BitArray

---
+++ PathReduxTests/Bits/BitArraySpanTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using YellowCounter.FileSystemState.Bits;
+
+namespace PathReduxTests.Bits
+{
+    [TestClass]
+    public class BitArraySpanTests
+    {
+        [TestMethod]
+        public void BitArraySpanCreate()
+        {
+            var x = new BitArray(new[] { false, true, true, false, true });
+
+            var s = x.ToSpan(1);
+
+            s[0].ShouldBe(true);
+            s[1].ShouldBe(true);
+            s[2].ShouldBe(false);
+            s[3].ShouldBe(true);
+
+        }
+
+        [TestMethod]
+        public void BitArraySpanOutOfRange()
+        {
+            Should.Throw(() =>
+            {
+                var x = new BitArray(new[] { false, true, true, false, true });
+                var s = x.ToSpan(1);
+                var q = s[4];
+            }, typeof(IndexOutOfRangeException));
+
+        }
+
+        [TestMethod]
+        public void BitArraySpanWrite()
+        {
+            var x = new BitArray(3);
+
+            var s = x.ToSpan(1);
+            s[0] = true;
+
+            x[0].ShouldBe(false);
+            x[1].ShouldBe(true);
+            x[2].ShouldBe(false);
+
+        }
+    }
+}
---
+++ YellowCounter.FileSystemState/Bits/BitArraySpan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YellowCounter.FileSystemState.Bits
+{
+    public ref struct BitArraySpan
+    {
+        private readonly BitArray array;
+        private readonly int start;
+        private readonly int length;
+
+        public BitArraySpan(BitArray array) : this(array, 0, array.Length) {}
+        public BitArraySpan(BitArray array, int start) : this(array, start, array.Length - start) { }
+
+        public BitArraySpan(BitArray array, int start, int length)
+        {
+            if(start < 0)
+                throw new ArgumentException(nameof(start));
+
+            if(length < 0)
+                throw new ArgumentException(nameof(length));
+
+            if(start + length > array.Length)
+                throw new ArgumentException(nameof(length));
+
+            this.array = array;
+            this.start = start;
+            this.length = length;
+        }
+
+        public bool this[int index]
+        {
+            get
+            {
+                verifyIndex(index);
+
+                return array[index + start];
+            }
+            set
+            {
+                verifyIndex(index);
+
+                array[index + start] = value;
+            }
+        }
+
+        private void verifyIndex(int index)
+        {
+            if(index < 0 || index >= length)
+                throw new IndexOutOfRangeException();
+        }
+    }
+
+    public static class BitArrayExtensions
+    {
+        public static BitArraySpan ToSpan(this BitArray bitArray) => new BitArraySpan(bitArray);
+        public static BitArraySpan ToSpan(this BitArray bitArray, int start) => new BitArraySpan(bitArray, start);
+        public static BitArraySpan ToSpan(this BitArray bitArray, int start, int length) => new BitArraySpan(bitArray, start, length);
+    }
+}
